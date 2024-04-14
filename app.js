b10f81e5348a9b2287fb32a9f6c65c8dc7d26c6e
brymmobaggins <ibro2tablet21@yahoo.com>
2024-04-14 23:12:46 +0100

    create account for username and password

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 
 let form = document.querySelector("form")
 
-form.onclick = function (e) {
-    console.log(e)
+form.addEventListener("submit", function (event) {
+    event.preventDefault()
+    createAccount()
+})
+
+let usernameInput = document.querySelector('#name')
+let emailInput = document.querySelector('#name')
+let passwordInput = document.querySelector('#name')
+
+
+// function createAccount(){
+
+// }
+
+function createAccount() {
+    let username = usernameInput.value
+    let password = passwordInput.value
+
+    if (localStorage.getItem(username)) {
+        alert("username is already taken")
+    } else {
+        // save username and password to local storage
+        localStorage.setItem(username, password)
+
+        // Hide form, show success message
+        document.getElementById("#signup-form").style.display = "none"
+        document.getElementById("#successMessage").style.display = "block"
+    }
+
 }
+
+
